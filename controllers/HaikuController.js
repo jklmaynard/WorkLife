7bccb44447bc8dc3f4f5dfd46c8856b70209a82d
James Maynard <jameskirkmaynard@gmail.com>
2015-03-10 15:51:10 -0700

    Add delete functionality to haiku controller.

---
+++ controllers/HaikuController.js
@@ -0,0 +1,10 @@
+WorkLife.HaikuController = Ember.ObjectController.extend({
+  actions: {
+    delete: function() {
+      if (confirm('Uh, yeah?')) {
+        this.get('model').destroyRecord();
+      };
+      this.transitionToRoute('home')
+    }
+  }
+});
