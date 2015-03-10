ceecdc878d03e307c89639439cc5ef0e981d355c
James Maynard <jameskirkmaynard@gmail.com>
2015-03-10 12:23:25 -0700

    Add post.hbs with delete functionality to postController.

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,3 +1,10 @@
 WorkLife.PostController = Ember.ObjectController.extend({
-
+  actions: {
+    delete: function() {
+      if (confirm('Sure about this?')) {
+        this.get('model').destroyRecord();
+      };
+      this.transitionToRoute('posts')
+    }
+  }
 });
