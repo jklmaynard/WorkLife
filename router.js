d443d45ce50a52da2e09940172ab6e3f844fb1e0
James Maynard <jameskirkmaynard@gmail.com>
2015-03-10 14:14:23 -0700

    Add haiku templates, controllers, models, routes.

--- router.js
+++ router.js
@@ -3,6 +3,7 @@ WorkLife.Router.map(function() {
   this.resource('new-post');
   this.resource('posts');
   this.resource('post', {path: '/:post_id'});
+  this.resource('new-haiku')
   this.resource('haikus');
   this.resource('haiku', {path: '/:haiku_id'});
 });
